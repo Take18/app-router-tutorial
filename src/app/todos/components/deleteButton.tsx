9f604f4e3a402a845d36bfa0af713d2ed96b2acd
Take <take@lograph.co.jp>
2023-08-13 18:02:21 +0900

    [add] Implement Delete Function (only viewing)

--- src/app/todos/components/deleteButton.tsx
+++ src/app/todos/components/deleteButton.tsx
@@ -2,14 +2,17 @@
 
 import { DeleteButton as DButton } from "@/app/common/button/deleteButton";
 import { Todo, deleteTodo } from "../contracts";
+import { useRouter } from "next/navigation";
 
 type Props = {
   todo: Todo;
 };
 
 export const DeleteButton = ({ todo }: Props) => {
+  const router = useRouter();
   const onClick = async () => {
     deleteTodo(todo.id);
+    router.refresh();
   };
   return <DButton onClick={onClick} />;
 };
