39d673072f7aef3c6564c1e99c8bc5bba323c889
Take <take@lograph.co.jp>
2023-08-13 10:18:01 +0900

    [add] Connect Next-Auth to Prisma

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,9 @@
+import { PrismaClient } from "@prisma/client";
 import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+
+const prisma = new PrismaClient();
 
 const handler = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
@@ -9,6 +13,7 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
     }),
   ],
+  adapter: PrismaAdapter(prisma),
 });
 
 export { handler as GET, handler as POST };
