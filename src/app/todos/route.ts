8689092245042697a783fc6d87275a429af4becf
Take <take@lograph.co.jp>
2023-08-13 11:40:09 +0900

    [add] Add Simple Page And API of Todos

---
+++ src/app/todos/route.ts
@@ -0,0 +1,11 @@
+import { getServerSession } from "next-auth";
+import { options } from "../auth/options";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+  const session = await getServerSession(options);
+
+  console.log(session?.user);
+
+  return NextResponse.json({ message: "ok" });
+};
